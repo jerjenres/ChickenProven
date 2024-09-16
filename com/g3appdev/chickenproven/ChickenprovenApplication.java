6769a8816587633c488c817071e37eb60db4358c
jerjenres <jerjenres.pangalay@cit.edu>
2024-09-16 23:36:04 +0800

    initial commit

---
+++ com/g3appdev/chickenproven/ChickenprovenApplication.java
@@ -0,0 +1,13 @@
+package com.g3appdev.chickenproven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChickenprovenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChickenprovenApplication.class, args);
+	}
+
+}
