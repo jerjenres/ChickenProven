1ef40a77dcdcd78b61c8859f2dafe40fddb0f351
jerjenres <jerjenres.pangalay@cit.edu>
2024-09-19 21:40:10 +0800

    crud

---
+++ src/test/java/com/g3appdev/chickenproven/ChickenprovenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g3appdev.chickenproven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChickenprovenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
